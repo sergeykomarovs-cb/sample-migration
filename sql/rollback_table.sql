ce34fa4ae6c21538486fd0309d1bd57a1db2623f
Sergey Komarov <sergey.komarov@cleverbase.com>
2021-04-01 16:41:15 +0300

    fix typo

--- sql/rollback_table.sql
+++ sql/rollback_table.sql
@@ -1,4 +1,4 @@
-DROP table verifiable_credential_event
+DROP table verifiable_credential_event;
 
 CREATE TABLE verifiable_credential_event AS 
-TABLE verifiable_credential_event_backup
+TABLE verifiable_credential_event_backup;
